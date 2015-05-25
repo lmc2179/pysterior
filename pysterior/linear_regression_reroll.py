49a0e445ad40ae156462fbc941bdbfe7cad725bc
Louis Cialdella <lmc2179@github.com>
2015-05-25 14:27:28 -0400

    Added classes

--- pysterior/linear_regression_reroll.py
+++ pysterior/linear_regression_reroll.py
@@ -3,55 +3,62 @@ import random
 from functools import reduce
 from scipy.stats.distributions import norm
 
-def propose(current):
-    PROPOSAL_VARIANCE = 0.1
-    return np.array([random.gauss(w_i, PROPOSAL_VARIANCE) for w_i in current]) #TODO: This can probably be done faster
+class MarkovChainSampler(object):
+    @staticmethod
+    def propose(current):
+        PROPOSAL_VARIANCE = 0.1
+        return np.array([random.gauss(w_i, PROPOSAL_VARIANCE) for w_i in current])
 
-def prior(w):
-    FIXED_PRECISION = 0.0001
-    probabilities = norm.pdf(w, 0, 1.0/FIXED_PRECISION)
-    return reduce(lambda x,y:x*y, probabilities, 1.0)
+    @staticmethod
+    def prior(w):
+        FIXED_PRECISION = 0.0001
+        probabilities = norm.pdf(w, 0, 1.0/FIXED_PRECISION)
+        return reduce(lambda x,y:x*y, probabilities, 1.0)
 
-def f(x,w):
-    return x.dot(w)
+    @staticmethod
+    def accept(prob_old, prob_new):
+        acceptance_prob = min(1, prob_new/prob_old)
+        if random.random() < acceptance_prob:
+            return True
+        else:
+            return False
 
-def accept(prob_old, prob_new):
-    acceptance_prob = min(1, prob_new/prob_old)
-    if random.random() < acceptance_prob:
-        return True
-    else:
-        return False
+    @classmethod
+    def parameter_likelihood(cls, w, true_t, expected_t):
+        FIXED_OBSERVATION_NOISE_VARIANCE = 1.0
+        return cls.prior(w)*norm.pdf(expected_t, true_t, FIXED_OBSERVATION_NOISE_VARIANCE)
 
-def get_random(X,y):
-    i = random.choice(range(len(X)))
-    return X[i], y[i]
+class BayesianLinearRegression(object):
+    @staticmethod
+    def get_posterior_samples(X, y, iterations, burn_in=None, thinning=None):
+        dimension = len(X[0])
+        w = np.zeros(dimension)
+        samples = []
+        sampler = MarkovChainSampler()
+        model = LinearRegressionModel()
+        for i in range(iterations):
+            for x,t in zip(X,y):
+                potential_w = sampler.propose(w)
+                expected_t = model.f(x,w)
+                expected_t_potential = model.f(x, potential_w)
+                prob_w = sampler.parameter_likelihood(w, t, expected_t)
+                prob_potential_w = sampler.parameter_likelihood(potential_w, t, expected_t_potential)
+                if sampler.accept(prob_w, prob_potential_w):
+                    w = potential_w
+                if burn_in and i > burn_in and thinning and i%thinning == 0:
+                    samples.append(w)
+        parameter_samples = zip(*samples)
+        for p in parameter_samples:
+            print(min(p),max(p))
+            print(np.sum(p)/len(p))
+            print(np.histogram(p))
 
-def parameter_likelihood(w, true_t, expected_t):
-    FIXED_OBSERVATION_NOISE_VARIANCE = 5.0
-    return prior(w)*norm.pdf(expected_t, true_t, FIXED_OBSERVATION_NOISE_VARIANCE)
+class LinearRegressionModel(object):
+    @staticmethod
+    def f(x,w):
+        return x.dot(w)
 
-def get_posterior_samples(X, y, iterations, burn_in=None, thinning=None):
-    dimension = len(X[0])
-    w = np.zeros(dimension)
-    samples = []
-    for i in range(iterations):
-        potential_w = propose(w)
-        x,t = get_random(X,y)
-        expected_t = f(x,w)
-        expected_t_potential = f(x, potential_w)
-        prob_w = parameter_likelihood(w, t, expected_t)
-        prob_potential_w = parameter_likelihood(potential_w, t, expected_t_potential)
-        if accept(prob_w, prob_potential_w):
-            w = potential_w
-        if burn_in and i > burn_in and thinning and i%thinning == 0:
-            samples.append(w)
-    print(sum(samples)/len(samples))
-    w1,w2 = zip(*samples)
-    print(min(w1),max(w1))
-    print(np.histogram(w1))
-    print(min(w2),max(w2))
-    print(np.histogram(w2))
-
+import cProfile
 X = np.array([[0,0],[0,1], [1,1], [50,2]])
 y = np.array([0,50.5,50.1, 101])
-get_posterior_samples(X,y, 50000, burn_in=30000, thinning=2)
+cProfile.run('BayesianLinearRegression.get_posterior_samples(X,y, 15000, burn_in=5000, thinning=2)')
