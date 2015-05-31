558aa4e0337463e6eceed8d9c702ba3bb8a6b19e
Louis Cialdella <lmc2179@github.com>
2015-05-31 19:26:00 -0400

    further optimize cythonized lognormpdf as pure c function

--- pysterior/norm_pdf.pyx
+++ pysterior/norm_pdf.pyx
@@ -3,8 +3,11 @@ cdef float pi = 3.141592653589793
 cdef extern from "math.h":
     float log(float x)
 
-def lognormpdf(float x, float mean, float sd):
+cdef float _lognormpdf(float x, float mean, float sd):
     cdef float var = sd**2
     cdef float denom = (2*pi*var)**.5
     cdef float num = -(float(x)-float(mean))**2/(2*var)
     return num - log(denom)
+
+def lognormpdf(float x, float mean, float sd):
+    return _lognormpdf(x, mean, sd)
