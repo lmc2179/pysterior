75a51e4c5d3ce38f43b3c6eada3421e0e00bca96
Louis Cialdella <lmc2179@github.com>
2015-06-04 20:48:18 -0400

    Added abstract classes for distributions

--- pysterior/samplers.py
+++ pysterior/samplers.py
@@ -2,19 +2,48 @@ from math import log
 import random
 import numpy as np
 
+class AbstractDistribution(object): #TODO: ABC Meta
+    def log_pdf(self, x):
+        raise NotImplementedError
+
+class AbstractProposalDistribution(object): #TODO: ABC Meta
+    def transition_log_probability(self, current_state, new_state):
+        raise NotImplementedError
+
+    def propose(self, current_state):
+        raise NotImplementedError
+
+def target_distribution_factory(log_pdf):
+    class TargetDistribution(AbstractDistribution):
+        def log_pdf(self, x):
+            return log_pdf(x)
+
+    return TargetDistribution()
+
+class GaussianProposalDistribution(AbstractProposalDistribution):
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def propose(self, current_state):
+        return np.random.normal(current_state, self.sigma)
+
 class MetropolisHastings(object):
     LOG_ONE = log(1.0)
-    def __init__(self, target_distribution):
-        self.target_distribution = target_distribution
+    def __init__(self, target_distribution, proposal_distribution):
+        if isinstance(target_distribution, AbstractDistribution):
+            self.target_distribution = target_distribution
+        else:
+            self.target_distribution = target_distribution_factory(target_distribution)
+        self.proposal_distribution = proposal_distribution
 
     def _propose(self, current_state):
-        raise NotImplementedError
+        return self.proposal_distribution.propose(current_state)
 
     def _evaluate_state(self, state):
-        return self.target_distribution(state)
+        return self.target_distribution.log_pdf(state)
 
     def _evaluate_proposal(self, current_state, new_state):
-        raise NotImplementedError
+        return self.proposal_distribution.transition_log_probability(current_state, new_state)
 
     def _get_initial_value(self):
         raise NotImplementedError
@@ -46,15 +75,12 @@ class MetropolisHastings(object):
 
 class Metropolis(MetropolisHastings):
     def _evaluate_proposal(self, current_state, new_state):
+        "Return log(1), because the transition probabilities are assumed to cancel since they are symmetric."
         return self.LOG_ONE
 
 class GaussianMetropolis1D(Metropolis):
     def __init__(self, proposal_variance, target_distribution):
-        self.proposal_sigma = proposal_variance
-        super(GaussianMetropolis1D, self).__init__(target_distribution)
-
-    def _propose(self, current_state):
-        return np.random.normal(current_state, self.proposal_sigma)
+        super(GaussianMetropolis1D, self).__init__(target_distribution, GaussianProposalDistribution(proposal_variance))
 
     def _get_initial_value(self):
         return random.gauss(0.0, 100)
