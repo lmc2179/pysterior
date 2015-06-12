1f8a93e9c65ff7c34b51de7c8614bd40647cc6c8
Louis Cialdella <lmc2179@github.com>
2015-06-11 21:17:32 -0400

    Refactored tests

--- pysterior/sampling/tests.py
+++ pysterior/sampling/tests.py
@@ -2,8 +2,8 @@ import unittest
 import pyximport
 import samplers
 import proposal_dist
-
 pyximport.install()
+import abc
 import pdfs
 
 #TODO: Exp direct sampling test
@@ -14,24 +14,53 @@ import pdfs
 
 #TODO: PosteriorParamter initial test
 
-class GaussianDirectSamplingTest(unittest.TestCase):
+@unittest.skip('')
+class OneDimensionalDirectSamplingTest(unittest.TestCase):
+    __meta__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def _get_target_log_pdf(self):
+        "Returns a function, which is the log_pdf of the target distribution."
+
+    @abc.abstractmethod
+    def _get_proposal_distribution(self):
+        "Returns a proposal distribution object."
+
+    @abc.abstractmethod
+    def _get_sampler_class(self):
+        "Provide a class which implements the a sample() method and takes a target/proposal distribution."
+
+@unittest.skip('')
+class GaussianDirectSamplingTest(OneDimensionalDirectSamplingTest):
+    "Test direct sampling from a 1D Gaussian. Provides a target distribution, but still requires a proposal and a sampler."
     MU, SIGMA = 10, 13.7
     def _get_target_log_pdf(self):
         pdf_closure = lambda x: pdfs.lognormpdf(x, self.MU, self.SIGMA)
         return pdf_closure
 
-    def _get_proposal_distribution(self):
-        return proposal_dist.GaussianMetropolisProposal(6.0)
+    def _build_sampler(self):
+        sampler_cls = self._get_sampler_class()
+        return sampler_cls(self._get_target_log_pdf(),  self._get_proposal_distribution())
 
-    def test_sampling(self):
-        sampler = samplers.MetropolisHastings(self._get_target_log_pdf(),
-                                              self._get_proposal_distribution())
+    def test_sampling(self): #TODO: This is overweight, and will be abstracted properly when we start testing other distributions
+        sampler = self._build_sampler()
         samples = sampler.sample(90000,50000,2, 32.3)
         sample_mean = sum(samples)/len(samples)
         sample_variance = 1.0*sum([(s - sample_mean)**2 for s in samples])/(len(samples)-1)
         self.assertAlmostEqual(sample_mean, self.MU, delta=0.5, msg='Sample mean does not approximate theoretical mean')
         self.assertAlmostEqual(sample_variance, self.SIGMA**2, delta=20.0,
                                msg='Sample mean does not approximate theoretical variance')
+
+class MHGaussianDirectSamplingTest(GaussianDirectSamplingTest):
+    def _get_sampler_class(self):
+        return samplers.MetropolisHastings
+
+    def _get_proposal_distribution(self):
+        return proposal_dist.GaussianMetropolisProposal(6.0)
+
+class DynamicMHGaussianDirectSamplingTest(GaussianDirectSamplingTest):
+    pass
+
 
 @unittest.skip('Skipped GaussianParameterInference until posterior sampling is finished')
 class GaussianParameterInference(unittest.TestCase):
