f279c500ece6688cce000c06d3f4d7fe8573ea4b
Louis Cialdella <lmc2179@github.com>
2015-06-22 23:24:16 -0400

    Refactored loose methods into LeapFrogIntegrator

--- pysterior/sampling/hamiltonian_prototype.py
+++ pysterior/sampling/hamiltonian_prototype.py
@@ -8,6 +8,49 @@ import theano.tensor as T
 import math
 
 LOG_ONE = math.log(1.0)
+
+def calculate_acceptance_probability(current_value, sampled_momentum, proposed_value,
+                                                                  proposed_momentum, target_energy):
+    metropolis_num = (target_energy(proposed_value) - (0.5*np.dot(proposed_momentum,proposed_momentum)))
+    metropolis_denom = (target_energy(current_value) - (0.5*np.dot(sampled_momentum,sampled_momentum)))
+    metropolis_factor = metropolis_num - metropolis_denom
+    return min(metropolis_factor, LOG_ONE)
+
+def accept(acceptance_probability):
+    return math.log(random.random()) < acceptance_probability
+
+class LeapfrogIntegrator(object):
+    def __init__(self, target_energy_gradient):
+        self.target_energy_gradient = target_energy_gradient
+
+    def _leapfrog_step(self, value, momentum, step_size):
+        half_step_momentum = momentum + (step_size*0.5*self.target_energy_gradient(value))
+        step_value = value + (step_size*half_step_momentum)
+        step_momentum = half_step_momentum + (step_size*0.5*self.target_energy_gradient(step_value))
+        return step_value, step_momentum
+
+    def run_leapfrog(self, current_value, current_momentum, num_steps, step_size):
+        value, momentum = current_value, current_momentum
+        for i in range(num_steps):
+            value,momentum = self._leapfrog_step(value, momentum, step_size)
+        return value, momentum
+
+def run_hamiltonian_sampling(initial_value, num_steps, step_size, target_energy, target_energy_gradient, iterations):
+    dimension = len(initial_value)
+    current_value = initial_value
+    integrator = LeapfrogIntegrator(target_energy_gradient)
+    samples = []
+    for i in range(iterations):
+        sampled_momentum = np.random.multivariate_normal(np.zeros(dimension), np.eye(dimension))
+        proposed_value, proposed_momentum = integrator.run_leapfrog(current_value, sampled_momentum, num_steps,
+                                                                    step_size)
+        acceptance_probability = calculate_acceptance_probability(current_value, sampled_momentum, proposed_value,
+                                                                  proposed_momentum, target_energy)
+        if accept(acceptance_probability):
+            samples.append(proposed_value)
+            current_value = proposed_value
+    return samples
+
 x = T.vector('x')
 mu = T.vector('mu')
 inv_cov_matrix = T.matrix('inv_cov_matrix')
@@ -23,43 +66,6 @@ def gaussian_log_pdf(X):
 
 def gaussian_log_gradient(X):
     return gaussian_energy_gradient(X, TRUE_MEAN, INV_COV)
-
-def calculate_acceptance_probability(current_value, sampled_momentum, proposed_value,
-                                                                  proposed_momentum, target_energy):
-    metropolis_num = (target_energy(proposed_value) - (0.5*np.dot(proposed_momentum,proposed_momentum)))
-    metropolis_denom = (target_energy(current_value) - (0.5*np.dot(sampled_momentum,sampled_momentum)))
-    metropolis_factor = metropolis_num - metropolis_denom
-    return min(metropolis_factor, LOG_ONE)
-
-def accept(acceptance_probability):
-    return math.log(random.random()) < acceptance_probability
-
-def leapfrog_step(value, momentum, step_size, energy_gradient):
-    half_step_momentum = momentum + (step_size*0.5*energy_gradient(value))
-    step_value = value + (step_size*half_step_momentum)
-    step_momentum = half_step_momentum + (step_size*0.5*energy_gradient(step_value))
-    return step_value, step_momentum
-
-def run_leapfrog(current_value, current_momentum, num_steps, step_size, target_energy_gradient):
-    value, momentum = current_value, current_momentum
-    for i in range(num_steps):
-        value,momentum = leapfrog_step(value, momentum, step_size, target_energy_gradient)
-    return value, momentum
-
-def run_hamiltonian_sampling(initial_value, num_steps, step_size, target_energy, target_energy_gradient, iterations):
-    dimension = len(initial_value)
-    current_value = initial_value
-    samples = []
-    for i in range(iterations):
-        sampled_momentum = np.random.multivariate_normal(np.zeros(dimension), np.eye(dimension))
-        proposed_value, proposed_momentum = run_leapfrog(current_value, sampled_momentum, num_steps, step_size,
-                                                         target_energy_gradient)
-        acceptance_probability = calculate_acceptance_probability(current_value, sampled_momentum, proposed_value,
-                                                                  proposed_momentum, target_energy)
-        if accept(acceptance_probability):
-            samples.append(proposed_value)
-            current_value = proposed_value
-    return samples
 
 samples = run_hamiltonian_sampling(np.array([100.0, 100.0]), 100, 0.1, gaussian_log_pdf, gaussian_log_gradient, 1000)
 plt.plot(*list(zip(*samples)), marker = '.', linewidth=0.0) #TODO: This is wrong - it looks like a random walk
