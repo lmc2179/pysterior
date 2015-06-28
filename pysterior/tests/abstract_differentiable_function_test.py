62e41c09fec042505022d8afb6a3005fdcbae659
Louis Cialdella <lmc2179@github.com>
2015-06-28 19:28:05 -0400

    added dot product test

--- pysterior/tests/abstract_differentiable_function_test.py
+++ pysterior/tests/abstract_differentiable_function_test.py
@@ -10,7 +10,18 @@ class SumOfSquares(abstract_differentiable_function.AbstractDifferentiableFuncti
         y = sum([x**2 for x in X])
         return X, [], y
 
+class DotProduct(abstract_differentiable_function.AbstractDifferentiableFunction):
+    def _get_variables(self):
+        x1 = T.vector('x1')
+        x2 = T.vector('x2')
+        y = T.dot(x1, x2)
+        return [x1], [x2], y
+
 class ConcreteDifferentiableFunctionTest(unittest.TestCase):
     def test_sum_squares(self):
         self.assertEqual(SumOfSquares().eval(1,2),1**2+2**2)
         self.assertEqual(SumOfSquares().gradient(1,2),[2*1, 2*2])
+
+    def test_dot_product(self):
+        self.assertEqual(DotProduct().eval([1,1], [1,1]), 2.0)
+        assert T.eq(DotProduct().gradient([1,1], [1,1]) , [1.0, 1.0])
