d2b4d8638443fb399340c1c4011bec1f28dba83b
Louis Cialdella <lmc2179@github.com>
2015-06-28 22:58:24 -0400

    Added StochasticGradientBatchDescent

--- pysterior/bayes_estimator.py
+++ pysterior/bayes_estimator.py
@@ -1,14 +1,22 @@
 import numpy as np
+import random
 
-class GradientDescent(object):
-    def minimize(self, target_function, starting_point, iterations, tol):
-        "Takes an instance of AbstractDifferentiableFunction, and runs minimization with unit learning rate."
-        LEARNING_RATE = 0.9
+class StochasticBatchGradientDescent(object):
+    def minimize(self, target_functions, starting_point, iterations, batch_size=1):
+        "Takes an instance of AbstractDifferentiableFunction, and runs minimization with set learning rate."
+        LEARNING_RATE = 0.6
         p = starting_point
         for i in range(iterations):
-            new_p = p - np.dot(target_function.gradient(p),LEARNING_RATE)
-            if abs(new_p-p) < tol:
-                return new_p
-            else:
-                p = new_p
+            target_function_batch = random.sample(target_functions, batch_size)
+            p = self._get_next_point_batch(target_function_batch, p, LEARNING_RATE)
+            LEARNING_RATE = LEARNING_RATE*0.99
         return p
+
+    def _get_next_point(self, target_function, p, learning_rate):
+        next_point = p - np.dot(target_function.gradient(p), learning_rate)
+        return next_point
+
+    def _get_next_point_batch(self, target_function_batch, p, learning_rate):
+        target_function_folded = lambda x: sum([function.gradient(x) for function in target_function_batch])
+        next_point = p - np.dot(target_function_folded(p), learning_rate)
+        return next_point
--- pysterior/tests/bayes_estimator_test.py
+++ pysterior/tests/bayes_estimator_test.py
@@ -6,13 +6,18 @@ os.path.join('..')
 import bayes_estimator
 
 class Exponential(abstract_differentiable_function.AbstractDifferentiableFunction):
+    def __init__(self, shift):
+        self.shift = float(shift)
+        super(Exponential, self).__init__()
+
     def _get_variables(self):
         x = T.scalar('x')
-        y = x**2
+        y = (x+self.shift)**2 + 1
         return [x], [], y
 
 class StochasticGradientDescentTest(unittest.TestCase):
     def univariate_convex_function_test(self):
-        optimizer = bayes_estimator.GradientDescent()
-        minimum = optimizer.minimize(Exponential(), 10.0, 100, 1e-10)
-        print(minimum)
+        optimizer = bayes_estimator.StochasticBatchGradientDescent()
+        functions = [Exponential(0), Exponential(3), Exponential(4), Exponential(5)]
+        minimum = optimizer.minimize(functions, starting_point=10.2, iterations=1000, batch_size=2)
+        self.assertAlmostEqual(minimum, -3.0, delta=0.05)
