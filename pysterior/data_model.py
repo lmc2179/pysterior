2cde59d62773e8584a4465024bce55e9f737ef9c
Louis Cialdella <lmc2179@github.com>
2015-07-02 17:04:25 -0400

    Added posterior sample data model

---
+++ pysterior/data_model.py
@@ -0,0 +1,29 @@
+import math
+
+class PosteriorSample(object):
+    def __init__(self):
+        self.samples = []
+        self.average = None
+
+    def get_samples(self):
+        return self.samples
+
+    def add_sample(self, new_sample):
+        self.samples.append(new_sample)
+        self._update_average(new_sample)
+
+    def _update_average(self, new_sample):
+        if not self.average:
+            self.average = new_sample
+        else:
+            self.average = self.average + ((new_sample - self.average)/len(self.samples))
+
+    def get_mean(self):
+        return self.average
+
+    def get_median(self):
+        #TODO: Make this an online calculation
+        center = (len(self.samples)-1)/2
+        left, right = math.floor(center), math.ceil(center)
+        sorted_data = sorted(self.samples)
+        return (sorted_data[left] + sorted_data[right])/2.0
