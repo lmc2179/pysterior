a3e42839ed2f4add118bdeaaadfed056c4d30610
Louis Cialdella <lmc2179@github.com>
2015-08-26 21:21:14 -0400

    Gaussian evidence factory

--- pysterior/spikes/energy_function_spikes.py
+++ pysterior/spikes/energy_function_spikes.py
@@ -11,6 +11,11 @@ def build_arg_closure(f, bound_kwargs, unbound_arg_name):
         return f(**full_kwargs)
     return partial_fxn
 
+def sum_map(functions):
+    def sum_mapped_function(*args, **kwargs):
+        return sum((f(*args, **kwargs)) for f in functions)
+    return sum_mapped_function
+
 class EnergyBuilder(object):
     def build_energy(self, *args, **kwargs):
         raise NotImplementedError
@@ -38,6 +43,17 @@ class SphericalGaussianPriorFactory(object):
         closed_gradient = build_arg_closure(gradient, closed_variables, 'X')
         return closed_total_energy, closed_gradient
 
+class GaussianEvidenceFactory(object):
+    def build_evidence_function(self, data):
+        gaussian_fxn_spec = energy.get_normal_spec()
+        factory = energy.PartiallyDifferentiableFunctionFactory(gaussian_fxn_spec)
+        f, mu_gradient = factory.get_partial_diff('mu')
+        _, sigma_gradient = factory.get_partial_diff('sigma')
+        evidence_fxn = sum_map([functools.partial(f, **{'X': d}) for d in data])
+        mu_evidence_gradient = sum_map([functools.partial(mu_gradient, **{'X': d}) for d in data])
+        sigma_evidence_gradient = sum_map([functools.partial(sigma_gradient, **{'X': d}) for d in data])
+        return evidence_fxn, mu_evidence_gradient, sigma_evidence_gradient
+
 class PriorFactoryTest(unittest.TestCase):
     def test_prior_construction(self):
         factory = SphericalGaussianPriorFactory()
@@ -46,6 +62,19 @@ class PriorFactoryTest(unittest.TestCase):
         self.assertEqual(f(np.array([1,1])), -1.0)
         self.assertEqual(list(grad(np.array([0,0]))), [ 0., 0.])
         self.assertEqual(list(grad(np.array([1,1]))), [-1., -1.])
+
+class GaussianEvidenceFactoryTest(unittest.TestCase):
+    def test_single_point_evidence(self):
+        factory = GaussianEvidenceFactory()
+        data = [np.array([0,0])]
+        f, mu_grad, sigma_grad = factory.build_evidence_function(data)
+        hyp_1 = {'mu': np.array([1.0, 1.0]), 'sigma': np.eye(2)}
+        hyp_2 = {'mu': np.array([-1.0, -1.0]), 'sigma': np.eye(2)}
+        hyp_0 = {'mu': np.array([0.0, 0.0]), 'sigma': np.eye(2)}
+        print(f(**hyp_1), mu_grad(**hyp_1), sigma_grad(**hyp_1))
+        print(f(**hyp_2), mu_grad(**hyp_2), sigma_grad(**hyp_2))
+        print(f(**hyp_0), mu_grad(**hyp_0), sigma_grad(**hyp_0))
+
 
 if __name__ == '__main__':
     unittest.main()
