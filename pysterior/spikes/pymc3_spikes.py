a7b59460cfa28af9f7ce43958a7ff7b2219ed010
Louis Cialdella <lmc2179@github.com>
2015-08-28 20:10:16 -0400

    generalize to n dimensions

--- pysterior/spikes/pymc3_spikes.py
+++ pysterior/spikes/pymc3_spikes.py
@@ -2,17 +2,18 @@
 import pymc3
 import numpy as np
 
+np.random.seed(123)
+
 # No. 1: Linear Regression
-def build_lr_model(X1, X2, Y):
+def build_lr_model(X, Y):
     lr_model = pymc3.Model()
 
     with lr_model:
         alpha = pymc3.Normal(name='alpha', mu=0, sd=20)
         beta = pymc3.Normal(name='beta', mu=0, sd=10, shape=2)
         sigma = pymc3.HalfNormal(name='sigma', sd=1)
-        X1 = pymc3.Normal(name='X1', mu=1, sd=2, observed=X1)
-        X2 = pymc3.Normal(name='X2', mu=1, sd=2, observed=X2)
-        mu = alpha + beta[0]*X1 + beta[1]*X2
+        X = pymc3.Normal(name='X', mu=1, sd=2, observed=X)
+        mu = alpha + beta.dot(X.T)
         Y_obs = pymc3.Normal(name='Y_obs', mu=mu, sd=sigma, observed=Y)
 
     return lr_model
@@ -24,7 +25,8 @@ X1 = np.linspace(0, 1, size)
 X2 = np.linspace(0,.2, size)
 Y = TRUE_ALPHA + TRUE_BETA[0]*X1 + TRUE_BETA[1]*X2 + np.random.randn(size)*TRUE_SIGMA
 
-lr_model = build_lr_model(X1, X2, Y)
+X = np.array(list(zip(X1, X2)))
+lr_model = build_lr_model(X, Y)
 
 map_estimate = pymc3.find_MAP(model=lr_model)
 
