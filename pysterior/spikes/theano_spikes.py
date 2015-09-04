ceb62db4ca522c448badde28d58f1dc5da192b57
Louis Cialdella <lmc2179@github.com>
2015-09-03 22:48:23 -0400

    softmax

--- pysterior/spikes/theano_spikes.py
+++ pysterior/spikes/theano_spikes.py
@@ -17,3 +17,12 @@ calculate_results = function([W, x], outputs=results, allow_input_downcast=True)
 test_W = np.array([[0,1,2],[0,1,2]])
 test_x = np.array([0,1,2])
 print(calculate_results(test_W, test_x))
+
+# No. 2: The softmax function
+x = T.vector('x')
+y = T.nnet.softmax(x)
+smax = function([x], y, allow_input_downcast=True)
+print(smax(np.array([0,1,2])))
+elements = [0,1,2]
+total = sum([exp(el) for el in elements])
+print([exp(el)/total for el in elements])
