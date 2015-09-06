f2afd8a58f90e5e008fd615c62faeca5f693bc51
Louis Cialdella <lmc2179@github.com>
2015-09-06 14:04:10 -0400

    added logistic regression, doesn't work yet

--- pysterior/classification.py
+++ pysterior/classification.py
@@ -2,8 +2,19 @@ from math import exp, e
 import pymc3
 from pysterior import linear_regression
 import numpy as np
+from theano import tensor as T
+from theano import map, function
 
-class BinaryLogisticRegressionModel(linear_regression.AbstractModel):
+class AbstractLogisticRegression(linear_regression.AbstractModel):
+    def get_predictive_posterior_samples(self, x):
+        raise NotImplementedError
+
+    def predict(self, x):
+        "Approximates the expected value of the output variable."
+        s = self.get_predictive_posterior_samples(x)
+        return sum(s) / len(s)
+
+class BinaryLogisticRegressionModel(AbstractLogisticRegression):
     def _build_model(self, X, y):
         lr_model = pymc3.Model()
         with lr_model:
@@ -17,8 +28,6 @@ class BinaryLogisticRegressionModel(linear_regression.AbstractModel):
 
         return lr_model
 
-
-
     def get_predictive_posterior_samples(self, x):
         "Obtain a sample of the output variable's distribution by running the sample variable values through the model."
         predictive_posterior_samples = []
@@ -26,7 +35,38 @@ class BinaryLogisticRegressionModel(linear_regression.AbstractModel):
             predictive_posterior_samples.append(1.0/(1.0 + e**(-bias - np.dot(x, w))))
         return predictive_posterior_samples
 
-    def predict(self, x):
-        "Approximates the expected value of the output variable."
-        s = self.get_predictive_posterior_samples(x)
-        return sum(s) / len(s)
+class LogisticRegressionModel(AbstractLogisticRegression):
+    def _build_model(self, X, y):
+        output_dimension = max(y)+1
+        lr_model = pymc3.Model()
+        with lr_model:
+            X = pymc3.Normal(name='X', observed=X)
+            precision = 0.00001
+            weights = pymc3.Normal(name='weights',
+                                   tau=precision,
+                                   shape=(output_dimension, self.input_data_dimension),
+                                   testval=T.zeros((output_dimension, self.input_data_dimension)))
+            bias_precision = 0.00001
+            biases = pymc3.Normal(name='biases', mu=0, tau=bias_precision, shape=output_dimension)
+            dots = weights.dot(X.T) # No bias!
+            p = T.nnet.softmax(dots + biases.T)
+            y_obs = pymc3.Categorical(p=p, name='y_obs', observed=y)
+
+        return lr_model, weights, biases
+
+    def fit(self, X, y, sampling_iterations):
+        X = self._force_shape(X)
+        self.input_data_dimension = len(X[0])
+        model, w, b = self._build_model(X, y)
+        with model:
+            self.map_estimate = pymc3.find_MAP(model=model, vars=[w, b])
+            step = pymc3.NUTS(scaling=self.map_estimate)
+            trace = pymc3.sample(sampling_iterations, step, start=self.map_estimate)
+        self.samples = trace
+
+    def get_predictive_posterior_samples(self, x):
+        "Obtain a sample of the output variable's distribution by running the sample variable values through the model."
+        predictive_posterior_samples = []
+        for bias, w in zip(self.samples['bias'], self.samples['w']):
+            predictive_posterior_samples.append(1.0/(1.0 + e**(-bias - np.dot(x, w))))
+        return predictive_posterior_samples
--- pysterior/tests/classification_tests.py
+++ pysterior/tests/classification_tests.py
@@ -1,11 +1,21 @@
 import unittest
 from pysterior import classification
+import numpy as np
 
 class LogisticRegressionTest(unittest.TestCase):
-    def test_log_reg(self):
+    def test_binary_log_reg(self):
         X = [[1, 2], [0, 1], [1, 0], [2, 0]]
-        y = [1, 1, 0, 0]
+        y = [1, 1,  0, 0]
         lr = classification.BinaryLogisticRegressionModel()
+        lr.fit(X, y, 1000)
+        for x in X:
+            print(lr.get_predictive_posterior_samples(x))
+            print(lr.predict(x))
+
+    def test_log_reg(self):
+        X = np.array([[1, 1], [-1, 1], [-1, 1], [-1, -1]])
+        y = [0, 1,  0, 1]
+        lr = classification.LogisticRegressionModel()
         lr.fit(X, y, 1000)
         for x in X:
             print(lr.get_predictive_posterior_samples(x))
