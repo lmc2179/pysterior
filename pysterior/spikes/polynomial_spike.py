25edfa6bacd864b4195e8a1fc4936fa8f7562a88
Louis Cialdella <lmc2179@github.com>
2015-09-13 15:06:48 -0400

    Polynomial preprocessor looks good

--- pysterior/spikes/polynomial_spike.py
+++ pysterior/spikes/polynomial_spike.py
@@ -1,5 +1,7 @@
 import unittest
 import numpy as np
+import itertools
+import functools
 
 class NondecreasingFiniteSumFixedSequenceEnumerator(object):
     "Enterprise edition."
@@ -73,13 +75,33 @@ class NondecreasingFiniteSumFixedSequenceEnumerator(object):
                 sequences.append(np.copy(seq))
         return sequences
 
+class PolynomialFeatureGenerator(object):
+    def __init__(self, degree, dimension, inclue_bias=True):
+        self.degree = degree
+        self.include_bias = inclue_bias
+        self._set_exponent_vectors(dimension)
 
+    def _set_exponent_vectors(self, size):
+        exponent_vectors = []
+        if self.include_bias:
+            exponent_vectors = [np.zeros(size)]
+        for i in range(1,self.degree+1):
+            base_exponents = NondecreasingFiniteSumFixedSequenceEnumerator().non_increasing_sequences(size, i)
+            all_exponents_nested = [list(set(itertools.permutations(exponent))) for exponent in base_exponents]
+            all_exponents = list(itertools.chain.from_iterable(all_exponents_nested))
+            vectorized_exponents = [np.array(x) for x in all_exponents]
+            exponent_vectors.extend(vectorized_exponents)
+        self.exponent_vectors = exponent_vectors
 
-def heap_permutations(L):
-    pass
+    def _get_polynomial_term(self, x, exponents):
+        product_terms = [base**ex for base,ex in zip(x, exponents)]
+        product = lambda x1, x2: x1*x2
+        return functools.reduce(product, product_terms)
 
-def preprocess_row_polynomial(row, degree, include_bias=True):
-    pass
+    def preprocess(self, row):
+        make_term_from_exponents = functools.partial(self._get_polynomial_term, row)
+        poly_row = np.array(list(map(make_term_from_exponents, self.exponent_vectors)))
+        return poly_row
 
 class SequenceTest(unittest.TestCase):
     def test_seq(self):
@@ -105,6 +127,11 @@ class SequenceTest(unittest.TestCase):
         expected = {(3,)}
         result_set = set(tuple([tuple(s) for s in sequences]))
         self.assertEqual(expected, result_set)
+
+    def test_quadratic(self):
+        x = np.array([2,5])
+        gen = PolynomialFeatureGenerator(2, len(x))
+        self.assertEqual(list(gen.preprocess(x)), [1, 5, 2, 4, 25, 10])
 
 if __name__ == '__main__':
     unittest.main()
