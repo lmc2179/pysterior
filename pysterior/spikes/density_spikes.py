1f7e8c9091dd55e55927601bede33621efd2d54a
Louis Cialdella <lmc2179@github.com>
2015-10-12 08:19:39 -0400

    logistic regression and mlp

--- pysterior/spikes/density_spikes.py
+++ pysterior/spikes/density_spikes.py
@@ -75,7 +75,60 @@ def test_poisson_analysis():
     # Type I and Type II error likelihoods
     plt.show()
 
+def test_logistic_regression():
+    data_X = np.array([[0,0],[1,0],[0,1],[1,1]])
+    data_y = np.array([0,1,0,1])
+
+    with pymc3.Model() as model:
+        X = pymc3.Normal(name='X', mu=1, sd=2, observed=data_X)
+        alpha1_precision = pymc3.Uniform(name='alpha1_precision')
+        alpha1 = pymc3.Normal(name='alpha1', mu=0, sd=1.0 / alpha1_precision)
+        alpha2_precision = pymc3.Uniform(name='alpha2_precision')
+        alpha2 = pymc3.Normal(name='alpha2', mu=0, sd=1.0 / alpha2_precision)
+        beta1_precision = pymc3.Uniform(name='beta1_precision')
+        beta2_precision = pymc3.Uniform(name='beta2_precision')
+        beta1 = pymc3.Normal(name='beta1', mu=0.0, sd=1.0 / beta1_precision, shape=2)
+        beta2 = pymc3.Normal(name='beta2', mu=0.0, sd=1.0 / beta2_precision, shape=2)
+        v1 = alpha1 + beta1.dot(X.T)
+        v2 = alpha2 + beta2.dot(X.T)
+        denom = T.exp(v1) + T.exp(v2)
+        sm1 = T.exp(v1) / denom
+        sm2 = T.exp(v2) / denom
+        v = T.stack(sm1, sm2)
+        y = pymc3.Categorical(p=v, name='y', observed=data_y, shape=len(data_y))
+
+        step = pymc3.Metropolis()
+        trace = pymc3.sample(1000, step)
+
+    pass
+
+def test_mlp():
+    true_beta = np.array([-3.0, 5.0])
+    true_alpha = 5.0
+    data_X = np.array(list(zip(np.linspace(-1.0, 1.0),np.linspace(-1.0, 1.0))))
+    data_y = np.array([true_beta.dot(x) + true_alpha for x in data_X])
+    n_inputs = 2
+    n_hidden = 5
+
+    with pymc3.Model() as model:
+        X = pymc3.Normal(name='X', mu=1, sd=2, observed=data_X)
+        w1_precision = pymc3.Uniform(name='w1_precision')
+        w1 = pymc3.Normal(name='w1', mu=0, sd=1.0 / w1_precision, shape=(n_hidden, n_inputs))
+        alpha_precision = pymc3.Uniform(name='alpha_precision')
+        alpha = pymc3.Normal(name='alpha', mu=0, sd=1.0 / alpha_precision, shape=n_hidden)
+        h = T.nnet.sigmoid(w1.dot(X.T) + alpha)
+        w2_precision = pymc3.Uniform(name='w2_precision')
+        w2 = pymc3.Normal(name='w2', mu=0, sd = 1.0 / w2_precision, shape=n_inputs)
+        output_noise_precision = pymc3.Uniform(name='output_noise_precision')
+        y = pymc3.Normal(name='y', mu=h.dot(w2.T), sd=1.0 / output_noise_precision, observed=data_y)
+
+        step = pymc3.Metropolis()
+        trace = pymc3.sample(1000, step)
+
+
 if __name__ == '__main__':
     # test_bernoulli()
     # test_categorical()
-    test_poisson_analysis()
+    # test_poisson_analysis()
+    # test_logistic_regression()
+    test_mlp()
